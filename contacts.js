fcfc58036f9782031b1509283d391d8d1c025d97
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-06-14 16:30:13 +0300

    adds contactsPath and listContacts

--- contacts.js
+++ contacts.js
@@ -1,28 +1,43 @@
-const fs = require('fs').promises;
+// const fs = require('fs').promises;
+const fs = require('fs/promises');
 const path = require('path');
+// const contactsPath = './db/contacts.json';
+const contactsPath = path.join(__dirname, 'db/contacts.json');
 
-const contactsPath = require('./db/contacts.json');
+// const contactsPath = async () => {
+//   await fs
+//     .readFile('./db/contacts.json')
+//     .then((data) => {
+//       console.log(data.toString());
+//       return data;
+//     })
+//     .catch((err) => console.log(err.message));
+// };
 
-export function listContacts() {
-  fs.readFile(contactsPath)
+const listContacts = async () => {
+  await fs
+    .readFile(contactsPath, 'utf-8')
     .then((data) => {
-      console.log(data.toString());
+      // console.log(data.toString());
+      console.log(data);
       return data;
+      // return JSON.parse(data);
     })
     .catch((err) => console.log(err.message));
-}
+};
 
-export function getContactById(contactId) {
-  fs.readFile(contactsPath)
+const getContactById = async (contactId) => {
+  await fs
+    .readFile(contactsPath, 'utf-8')
     .then((data) => {
-      const foundedContact = data.filter((contact) => contactId === contact.id);
+      const foundedContact = [...data].filter((contact) => contactId === contact.id);
       console.log(foundedContact);
       return foundedContact;
     })
     .catch((err) => console.log(err.message));
-}
+};
 
-export function removeContact(contactId) {
+function removeContact(contactId) {
   fs.readFile(contactsPath)
     .then((data) => {
       const removingContact = data.filter((contact) => contactId === contact.id);
@@ -34,18 +49,23 @@ export function removeContact(contactId) {
     .catch((err) => console.log(err.message));
 }
 
-export function addContact(name, email, phone) {
-  const addingContact = { name, email, phone };
+// function addContact (name, email, phone) {
+//   const addingContact = { name, email, phone };
+// fs.appendFile(contactsPath, addingContact)
+//   fs.readFile(contactsPath)
+//     .then((data) => {
+//       const afterAddContact = [...data, addingContact];
+//       fs.writeFile(contactsPath, afterAddContact);
+//       console.log(addingContact, afterAddContact);
+//       return addingContact;
+//     })
+//     .catch((err) => console.log(err.message));
+// };
 
-  fs.readFile(contactsPath)
-    .then((data) => {
-      const afterAddContact = [...data, addingContact];
-      fs.writeFile(contactsPath, afterAddContact);
-      console.log(addingContact, afterAddContact);
-      return addingContact;
-    })
-    .catch((err) => console.log(err.message));
-}
+// const contactFunctions = { listContacts, addContact, removeContact, getContactById };
+// export default listContacts;
+
+module.exports = { listContacts, getContactById };
 
 // fs.readFile(filename, [options]) - читання файлу
 // fs.writeFile(filename, data, [options]) - запис файлу
--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-import { removeContact, addContact, listContacts, getContactById } from './contacts';
+const contactFunctions = require('./contacts');
 
-listContacts();
+// import contactFunctions from './contacts.js';
+
+contactFunctions.listContacts();
+
+//  import { getContactById } from './contacts.js';
+
+// const listContacts = require('listContacts');
+// const removeContact = require('removeContact');
+// const addContact = require('addContact');
+// const getContactById = require('getContactById');
+
+// contactFunctions.getContactById('e6ywwRe4jcqxXfCZOj_1e');
